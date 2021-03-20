f9043c83f6531a9df3bb0944b049ea7eed9aa872
Anthony Manotoa <aimm1999@gmail.com>
2021-03-20 11:21:02 -0500

    add list comprehensions

---
+++ list_comprehensions.py
@@ -0,0 +1,20 @@
+# lista de los primeros 100 numeros naturales al cuadrado mientas no sean divisibles para 3
+
+
+def run():
+    # natural_numbers = []
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         natural_numbers.append(i**2)
+    # return natural_numbers
+
+    # natural_numbers = [i**2 for i in range(1, 101) if i % 3 != 0]
+    # return natural_numbers
+    
+    # RETO: lista de 1 hasta max 5 digitos que sean multiplos de 4, 6 y 9.
+    reto = [i for i in range(1, 100_000) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0]
+    return reto
+
+
+if __name__ == '__main__':
+    print(run())
---
+++ lists_and_dicts.py
@@ -0,0 +1,32 @@
+def run():
+    my_list = [1, 'Hello', True, 4.5]
+    my_dic = {'fistname': 'Tony', 'lastname': 'Manotoa'}
+
+    super_list = [
+        {'fistname': 'Tony', 'lastname': 'Manotoa'},
+        {'fistname': 'Angie', 'lastname': 'Paladines'},
+        {'fistname': 'Max', 'lastname': 'Medina'},
+        {'fistname': 'Jandry', 'lastname': 'Camacho'},
+        {'fistname': 'David', 'lastname': 'Beckam'}
+    ]
+
+    super_dic = {
+        'natural_nums': [1, 2, 3, 4, 5],
+        'integer_num': [-1, -2, 0, 1, 2],
+        'float_nums': [1.1, 4.5, 6.43]
+    }
+
+    for key, value in super_dic.items(): # con .items() imprimes tanto el value como la key
+        print(key, '-', value)
+
+    print('\n' + '*' * 50 + '\n')
+
+    for dicts in super_list:
+        for key, value in dicts.items():
+            print(f'{key} - {value}')
+        print('-' * 50)
+    
+    print(my_list, my_dic)
+
+if __name__ == '__main__':
+    run()
