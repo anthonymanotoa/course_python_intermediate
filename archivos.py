999ebe3d1c6f038f9abaa71bafd4b1b66c05d3ab
Anthony Manotoa <aimm1999@gmail.com>
2021-03-22 12:01:05 -0500

    Aprendi a trabajar con archivos, a leerlos, sobreescribirlos y hacerles append

--- archivos.py
+++ archivos.py
@@ -1,13 +1,21 @@
 def read():
-    pass
+    numbers = []
+    with open('./archivos/numbers.txt', 'r', encoding='utf-8') as f:
+        for i in f:
+            numbers.append(int(i))
+    print(numbers)
 
 
 def write():
-    pass
+    names = ['Tony', 'Miguel', 'Pepe', 'Christian', 'Rocío']
+    with open('./archivos/names.txt', 'a', encoding='utf-8') as f:
+        for name in names:
+            f.write(name)
+            f.write('\n')
 
 
 def run():
-    pass
+    write()
 
 
 if __name__ == '__main__':
