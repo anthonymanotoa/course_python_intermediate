9d010bde30e3d0b6408955fc36fa6a493963c208
Anthony Manotoa <aimm1999@gmail.com>
2021-03-23 23:46:51 -0500

    Implemente list comprehensions y funciona una sola vez el input de la palabra y luego se genera otra palabra

--- hangman_game.py
+++ hangman_game.py
@@ -2,7 +2,7 @@
 # _ _ _ _ _ _
 # Ingresa una letra:
 # La consola se debe reiniciar cada que ingrese una palabra
-# Incorpora list o dic comprehensions, manejo de errores, manejo de archivos, 
+# Incorpora list o dic comprehensions, manejo de errores, manejo de archivos
 # Investiga la función enumerate (documentacion)
 # El método get de los diccionarios te puede servir
 # Con os.system("cls") puedes limpiar la pantalla
@@ -13,23 +13,38 @@
 import os
 import random
 
+
 def read_words():
-    word = []
-    with open('./words.txt', 'r', encoding='utf-8') as f: 
-        for words in f:
-            word.append(words)
-    return word[random.randint(0, len(word))]
+
+    with open('./words.txt', 'r', encoding='utf-8') as f:
+        list_words = [i for i in f]
+        word = list_words[random.randint(0, len(list_words))]
+
+    return word
 
 
-def ahorcado():
-    for _ in read_words():
-        print('_', end=' ')
+def ahorcado(letter):
+    magic_word = read_words()
+    if letter in magic_word:
+        for i in range(len(magic_word)):
+            if letter == magic_word[i]:
+                print(letter, end=' ')
+            else:
+                print('_', end=' ')
+    else:
+        for _ in magic_word:
+            print('_', end=' ')
 
 
 def run():
-    os.system('cls')
-    print('¡Adivina la palabra!')
-    print(ahorcado())
+    letter = ''
+    count = 0
+    while count < 10:
+        os.system('cls')
+        print('¡Adivina la palabra!')
+        print(ahorcado(letter))
+        letter = input('Escoge una letra: ').lower()
+        count += 1
 
 
 if __name__ == '__main__':
