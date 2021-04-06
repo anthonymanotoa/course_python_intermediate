13681ee0ee943b7b309d00754a8ebd996545cc67
Anthony Manotoa <aimm1999@gmail.com>
2021-04-06 11:13:13 -0500

    implementé manejo de errores y dejé el código en una posibile versión final

--- hangman_game.py
+++ hangman_game.py
@@ -1,12 +1,6 @@
-# ¡Adivina la palabra!
-# _ _ _ _ _ _
-# Ingresa una letra:
-# La consola se debe reiniciar cada que ingrese una palabra
 # Incorpora list o dic comprehensions, manejo de errores, manejo de archivos
 # Investiga la función enumerate (documentacion)
 # El método get de los diccionarios te puede servir
-# Con os.system("cls") puedes limpiar la pantalla
-# Anade un sistema de puntos
 # Dibuja el ahorcado con el codigo ascii y dibujar en pantalla
 # Mejora la interfaz, emojis
 
@@ -32,42 +26,45 @@ def game(magic_word, letter, game_word):
     return ' '.join(game_word)
 
 
-def game_print(magic_word, letter, game_word):
-    pass #podrias poner parametros boleanos para saber si estas continuando el jeugo o si ya se acabo
-
-
 def run():
     lives = 5
-    letter = ''
+    letter = ' '
     magic_word = read_words()
     game_word = ['_' for i in range(len(magic_word) - 1)]
 
     while lives > 0:
-        if game(magic_word, letter, game_word).count('_') > 0:
-            if letter in game(magic_word, letter, game_word):
-                os.system('cls')
-                print(f'Vidas restantes: {"❤" * (lives)}')
-                print('¡Adivina la palabra!')
-                print(game(magic_word, letter, game_word))
-                letter = input('Escoge una letra: ').lower()
+        try:
+            if len(letter) > 1:
+                raise ValueError('Solo puedes ingresar una letra')
             else:
-                lives -= 1
-                if lives > 0:
-                    os.system('cls')
-                    print(f'Vidas restantes: {"❤" * (lives)}')
-                    print('¡Adivina la palabra!')
-                    print(game(magic_word, letter, game_word))
-                    letter = input('Escoge una letra: ').lower()
+                if game(magic_word, letter, game_word).count('_') > 0:
+                    if letter in game(magic_word, letter, game_word):
+                        os.system('cls')
+                        print(f'Vidas restantes: {"❤" * (lives)}')
+                        print('¡Adivina la palabra!')
+                        print(game(magic_word, letter, game_word))
+                        letter = input('Escoge una letra: ').lower()
+                    else:
+                        lives -= 1
+                        if lives > 0:
+                            os.system('cls')
+                            print(f'Vidas restantes: {"❤" * (lives)}')
+                            print('¡Adivina la palabra!')
+                            print(game(magic_word, letter, game_word))
+                            letter = input('Escoge una letra: ').lower()
+                        else:
+                            os.system('cls')
+                            print('¡Te quedaste sin vidas!\nJuego terminado...\n')
+                            print('La palabra era: ' + magic_word)
                 else:
                     os.system('cls')
-                    print('¡Te quedaste sin vidas!\nJuego terminado...\n')
-                    print('La palabra era: ' + magic_word)
-        else:
-            os.system('cls')
-            print('¡Adivina la palabra!')
-            print(game(magic_word, letter, game_word))
-            print('¡Ganaste! √')
-            break
+                    print('¡Adivina la palabra!')
+                    print(game(magic_word, letter, game_word))
+                    print('¡Ganaste! √')
+                    break
+
+        except ValueError as ve:
+            return print(ve)
     
 
 if __name__ == '__main__':
